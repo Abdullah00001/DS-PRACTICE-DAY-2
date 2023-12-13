fab79db7e1b2dde24407f79cc283ba20ea05140f
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-13 18:49:40 +0600

    Solve Prefix Sum Problem

--- Duplicate.cpp
+++ Duplicate.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{ 
+{  
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
     
     return 0;
 }
---
+++ Get_Prefix_Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    long long A[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    long long pre[N];
+    pre[0] = A[0];
+    for (int i = 1; i < N; i++)
+    {
+        pre[i] = A[i] + pre[i - 1];
+    }
+    for (int i = N-1;i>=0; i--)
+    {
+        cout << pre[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}

2c79b67c33591fa3979e435bc634d1a40e289d2d
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-13 19:09:21 +0600

    solved

--- Sorted.cpp
+++ Sorted.cpp
@@ -13,6 +13,24 @@ int main()
         {
             cin >> A[i];
         }
+        bool sorted = true;
+        for (int i = 0; i < N-1; i++)
+        {
+            if (A[i] > A[i + 1])
+            {
+                sorted = false;
+                break;
+            }
+        }
+        if (sorted)
+        {
+            cout << "YES" << endl;
+        }
+
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }

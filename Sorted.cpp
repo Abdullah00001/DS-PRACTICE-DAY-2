b48877387bd168b6ada6152a5acc55a4727f9979
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-13 18:56:21 +0600

    sorted problem solving

---
+++ Sorted.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int N;
+        cin >> N;
+        long long A[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
+    }
+    return 0;
+}

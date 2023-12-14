08c4faea7659bb2297be28f70e32d63bfd225a43
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-14 09:14:46 +0600

    solved the duplicate problem

--- Duplicate.cpp
+++ Duplicate.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{  
+{
     int n;
     cin >> n;
     vector<long long> a(n);
@@ -9,6 +9,25 @@ int main()
     {
         cin >> a[i];
     }
-    
+    bool flag = false;
+    for (int i = 0; i < n; i++)
+    {
+        auto it = find(a.begin() + i + 1, a.end(), a[i]);
+        if (it != a.end())
+        {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
--- Printing_X.cpp
+++ Printing_X.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 { 
+    int N;
+    cin>>N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            /* code */
+        }
+        
+    }
     
     return 0;
 }
